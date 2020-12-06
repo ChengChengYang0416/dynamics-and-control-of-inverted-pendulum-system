5ccc75bbfa768aaab9e6d12e4fd0f83ead0def06
ChengChengYang1997 <kb240608@gmail.com>
2020-12-06 22:22:56 +0800

    create init.m to initialize the parameters and gains

---
+++ init.m
@@ -0,0 +1,16 @@
+% gravity
+g = 9.81;
+
+% parameters of the cart and the pendulum
+Mc = 2.4;
+mp = 0.23;
+lp = 0.36;
+
+% PID gains
+pid_c.p = 1.0;
+pid_c.d = 1.0;
+pid_c.i = 0.0;
+
+pid_d.p = 1.0;
+pid_d.d = 1.0;
+pid_d.i = 0.0;
