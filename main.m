0de527ab87e2e5dcb07424f66983dd91e20e9a0c
ChengChengYang1997 <kb240608@gmail.com>
2020-12-06 22:35:35 +0800

    resize the states and set the desired pose

--- main.m
+++ main.m
@@ -9,13 +9,22 @@ sim_t = 50;
 t = 0:delta_t:sim_t;
 
 % initialize the state variables of the inverted pendulem system
-theta = zeros(2, length(t)+1);
-theta_dot = zeros(2, length(t)+1);
-theta_ddot = zeros(2, length(t));
-x =zeros(2, length(t)+1);
-x_dot = zeros(2, length(t)+1);
-x_ddot = zeros(2, length(t));
-Fx = zeros(2, length(t));
-theta_degree = zeros(2, length(t)+1);
-theta_error_now = zeros(2, 1);
-theta_error_accu = zeros(2, 1);
+theta = zeros(1, length(t)+1);
+theta_dot = zeros(1, length(t)+1);
+theta_ddot = zeros(1, length(t));
+x =zeros(1, length(t)+1);
+x_dot = zeros(1, length(t)+1);
+x_ddot = zeros(1, length(t));
+Fx = zeros(1, length(t));
+theta_degree = zeros(1, length(t)+1);
+theta_error_now = zeros(1, 1);
+theta_error_accu = zeros(1, 1);
+
+% desired position and angle
+desired_theta = 0.0;
+desired_x = 0.1;
+
+% simulation start
+for i = 1:length(t)
+    
+end
