e39d87b21b064d6258aa93d4d06a2bdaa3cba595
ChengChengYang1997 <kb240608@gmail.com>
2020-12-17 00:57:22 +0800

    modify the desired states and initial condition, and add the dynamics

--- main.m
+++ main.m
@@ -9,14 +9,14 @@ t = 0:dt:sim_t;
 t = t';
 
 % desired states
-Xd = [0.1; 0; 0; 0];
+Xd = [0.5; 0; 0; 0];
 
 % initialize the state variables of the inverted pendulem system
 X = zeros(4, length(t));
 F = zeros(1, length(t));
 
 % initial condition
-X0   = [0; 0; 0; 0];
+X0   = [0; 0; 10*pi/180; 0];
 X(:, 1) = X0;
 iter = 2;
 
@@ -26,7 +26,7 @@ eX_dot = zeros(2, length(t));
 eX_int = zeros(2, length(t));
 
 for i = dt:dt:sim_t
-    eX(1, iter) = Xd(1) - X(1, iter-1);  
+    eX(1, iter) = Xd(1) - X(1, iter-1);
     eX_dot(1, iter)  = (eX(1, iter) - eX(1, iter-1))/dt;
     eX_int(1, iter)  = eX_int(1, iter-1) + eX(1, iter)*dt;
 
@@ -37,5 +37,7 @@ for i = dt:dt:sim_t
     F(iter) = -(pid_c.p*eX(1, iter) + pid_c.d*eX_dot(1, iter) + pid_c.i*eX_int(1, iter))...
                 + (pid_p.p*eX(2, iter) + pid_p.d*eX_dot(2, iter) + pid_p.i*eX_int(2, iter));
 
+    [T, X_new] = ode45(@(t, x) dynamics(t, x, F(iter)), [0, dt], X(:, iter-1)', F(iter));
+    X(:, iter) = X_new(end,:)';
     iter = iter + 1;
 end
